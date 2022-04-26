7f7f9319d5331d18d188c83c693b10661ebc4c34
xiaofeng <veraqxf@gmail.com>
2022-04-26 13:28:09 +0800

    feat

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,10 @@
 # 构建执行任务
 npm run build
 htmldir="/usr/share/nginx/html"
-uploadbasedir="${htmldir}/upgrade_vue_app"
+uploadbasedir="${htmldir}/upgrade_vue"
 appenddir=$1
 uploaddir="${uploadbasedir}/${appenddir}"
-projectdir="${htmldir}/vue_app"
+projectdir="${htmldir}/dist"
 scp -r ./dist/ root@49.233.26.253:${uploaddir}
 ssh root@49.233.26.253 > /dev/null 2>&1 <<eeooff
 ln -snf ${uploaddir} ${projectdir}
