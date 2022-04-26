92dacaba1d7ab41b516e12094ce1c451647ad4b7
xiaofeng <veraqxf@gmail.com>
2022-04-26 14:22:29 +0800

    feat(deploy.sh): deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,9 @@ uploadbasedir="${htmldir}/upgrade_blog_vue_ts"
 appenddir=$1
 uploaddir="${uploadbasedir}/${appenddir}"
 projectdir="/usr/share/nginx/html/blog_vue_ts"
-scp -r ./dist/. root@49.233.26.253:${uploaddir}
-ssh root@49.233.26.253 > /dev/null 2>&1 << eeooff
+txcloud=root@49.233.26.253
+scp -r ./dist/. ${txcloud}:${uploaddir}
+ssh ${txcloud} > /dev/null 2>&1 << eeooff
 ln -snf ${uploaddir} ${projectdir}
 exit
 eeooff
