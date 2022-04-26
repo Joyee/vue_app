517e3de6141df52b042e750dcac8093255d92c9d
xiaofeng <veraqxf@gmail.com>
2022-04-26 14:23:39 +0800

    feat(deploy.js): deploy.js

--- deploy.js
+++ deploy.js
@@ -1,7 +1,7 @@
 const { execFile } = require('child_process')
 const version = process.env.npm_package_version
 
-execFile('deploy.sh', [version], { shell: true }, (error, stdout) => {
+execFile('./deploy.sh', [version], { shell: true }, (error, stdout) => {
   if(error) {
     throw error
   }
