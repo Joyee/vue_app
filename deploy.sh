86d3193acb87dfee5d47f274c6573216510ebf42
xiaofeng <veraqxf@gmail.com>
2022-04-26 14:05:38 +0800

    feat(change deploy.sh): change deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-# 构建执行任务
 npm run build
 htmldir="/usr/share/nginx/html"
-uploadbasedir="${htmldir}/upgrade_vue"
+uploadbasedir="${htmldir}/upgrade_blog_vue_ts"
 appenddir=$1
 uploaddir="${uploadbasedir}/${appenddir}"
-projectdir="${htmldir}/dist"
-scp -r ./dist/ root@49.233.26.253:${uploaddir}
-ssh root@49.233.26.253 > /dev/null 2>&1 <<eeooff
+projectdir="/usr/share/nginx/html/blog_vue_ts"
+scp -r ./dist/. root@49.233.26.253:${uploaddir}
+ssh root@49.233.26.253 > /dev/null 2>&1 << eeooff
 ln -snf ${uploaddir} ${projectdir}
 exit
 eeooff
-echo done
+echo done!!!
